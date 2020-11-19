9b7eccb587e7201e1508be6d5696bf8dd4a25efd
Nsindiso Matshanga <nsindiso.matshanga@yahoo.com>
2020-11-19 08:46:03 -0800

    bug fix 21345 and sip

---
+++ JSON.py
@@ -0,0 +1,17 @@
+import json 
+
+with open("JSON.json") as data:
+    json_data = data.read()
+
+json_dict = json.loads(json_data)
+
+print(json_dict['People'][0]['Id'])
+
+json_dict['People'][0]['Id'] = 5
+
+with open("JSON.json", "w") as fh:
+    json.dump(json_dict, fh, indent=4)
+
+    
+
+
